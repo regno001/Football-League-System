94551e4865ceac9e817ded39ae01503f9315dc39
regno001 <rwt7454@gmail.com>
2025-06-25 21:37:57 +0530

    Add files via upload

---
+++ admin/partials/menu.php
@@ -0,0 +1,37 @@
+<?php include('../config/constants.php'); 
+include('login_check.php'); 
+?>
+
+<html>
+
+<head>
+
+<title>Cofo Admin</title>
+
+<link rel="stylesheet" href="../css/admin.css?v=<?php echo time(); ?>">
+
+</head>
+
+<div class="menu text-center">
+
+<div class="wrapper">
+
+<ul>
+
+<li><a href="index.php">Home</a></li>
+
+<li><a href="manage-admin.php">Admin</a></li>
+
+<li><a href="manage-category.php">Teams</a></li>
+
+<li><a href="manage-food.php">Matches</a></li>
+
+<li><a href="manage-scorecard.php">Scorecard</a></li>
+
+<li><a href="logout.php">Logout</a></li>
+
+</ul>
+
+</div>
+
+</div>
